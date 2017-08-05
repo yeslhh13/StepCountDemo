8654662f4d6b65a5cd784f0ca87abdaa13a13239
yeslhh13 <gowonkid@naver.com>
2017-08-05 11:06:22 +0900

    added soyeon's code(settings)

--- app/src/main/java/com/example/android/stepcountdemo/setting/SettingActivity.java
+++ app/src/main/java/com/example/android/stepcountdemo/setting/SettingActivity.java
@@ -1,20 +1,40 @@
 package com.example.android.stepcountdemo.setting;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.android.stepcountdemo.R;
 
+//import com.example.android.stepcountdemo.ContentViewActivity;
+
 /**
  * Created by soyeon on 2017-05-25.
- * 소연이는 그냥 클래스파일 우선 생성만 했음.. 구현은 나중에!
+ * 2017-08-03 last로 만짐
  */
 
 public class SettingActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+        setContentView(R.layout.activity_setting); //preference setting파일 새로 만들었음
     }
 
+    public static class GreenPreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_settings);
+        }
+
+        public boolean onPreferenceChange(Preference pref, Object newVal) {
+            String val = newVal.toString();
+            if (pref instanceof ListPreference) {
+                ListPreference listPref = (ListPreference) pref;
+                //int pref
+            }
+            return true;
+        }
+    }
 }
